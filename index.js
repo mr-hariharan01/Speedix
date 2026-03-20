adc4af7f6d135e36b924631bba285949661c1110
Hariharan <hariharan81416@gmail.com>
2026-03-20 22:49:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Speedix Server Running 🚀");
+  res.send("Speedix is LIVE 🚀🔥");
 });
 
 app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
